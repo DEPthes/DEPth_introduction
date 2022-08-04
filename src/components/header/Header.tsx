c3a02dbe7a586d76af48bdc5c56915ccc5b1050b
이가은 <0314kelee@naver.com>
2022-08-04 16:07:35 +0900

    [feat] 네비게이션 바

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 export const Header: React.FC = () => {
-    return <Inner>머가리</Inner>;
+    return (
+        <ContlorBox>
+            <Contlor
+                to="/"
+                className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+            >
+                ABOUT
+            </Contlor>
+            <Contlor
+                to="/Project"
+                className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+            >
+                PROJECT
+            </Contlor>
+            <Contlor
+                to="/Member"
+                className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+            >
+                MEMBER
+            </Contlor>
+            <Contlor
+                to="/Contact"
+                className={({ isActive }) => (isActive ? 'active' : 'inactive')}
+            >
+                CONTACT
+            </Contlor>
+        </ContlorBox>
+    );
 };
 
-const Inner = styled.div`
-    color: black;
+const ContlorBox = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    padding-top: 46px;
+    padding-bottom: 392px;
+`;
+
+export const Contlor = styled(NavLink)`
+    text-decoration: none;
+    font-size: 24px;
+    font-family: 'Sandoll Rotary';
+    font-style: normal;
+    font-weight: 400;
+    line-height: 25px;
+    color: rgba(255, 255, 255, 0.67);
+    margin: 18px;
+    position: relative;
+    &.active {
+        color: #ffffff;
+    }
+    &:hover {
+        color: #ffffff;
+    }
 `;
